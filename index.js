158dc18a604b5f5ef3f49b15ae3682b619b330f2
mccleskeyc <mccleskeyc@gmail.com>
2021-02-22 12:15:51 -0500

    commit tree ae9c4add7b7eed36eef1785832a45979af361230

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener( "DOMContentLoaded", function () {
+    updateText()
+  } );
+  
+  
+  function updateText() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
